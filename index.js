60ee6b1e64ff5e3002417451a740bb241330f2cb
mausi <manster161@gmail.com>
2022-09-15 16:56:32 +0200

    Adds websocket client

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
+const { connect } = require('http2');
+const { connection } = require('websocket');
+
 var express = require('express'),
     path = require('path'),
     app = express(),
-    mormalmidi = require('mormalmidi');
+    mormalmidi = require('mormalmidi'),
+    websocketserver = require('websocket').server,
+    http = require('http');
+
+var clientConnection;
+
+  const server = http.createServer(function(request,response){
+    console.log((new Date()) + ' Recieved request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+  }); 
+
+  server.listen(8081, function(){
+    console.log((new Date()) + ' Server is listening on port 8081');
+  });
+
+  const wsServer = new websocketserver({
+      httpServer: server,
+      autoAcceptConnections: false
+  });
+
+  function originIsAllowed(origin){
+    return true;
+  }
+
+  wsServer.on('request', function(request){
+    if (!originIsAllowed(request.origin)){
+      request.reject();
+      console.log((new Date()) + ' Connection from Origin' + request.origin + ' not accepted');
+      return;
+    }
+    
+    clientConnection = request.accept(null, request.origin);
+  
+    console.log((new Date()) + ' Connection request from' + request.origin + ' accepted');
+  });
+
+
+  function sendMessage(message){
+    clientConnection.send(message);
+  }
 
 mormalmidi.init();
+
+mormalmidi.addMapping(80, cc80);
+
+function cc80(value){
+  sendMessage(value);
+}
+
+
 
 app.set('port', (process.env.PORT || 8080));
 
--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,41 @@
+const serverURL = "ws://localhost:8081";
+let socket;
+
+function setup(){
+ openSocket(serverURL);
+}
+
+function openSocket(url){
+	socket = new WebSocket(url);
+	socket.addEventListener('open', openConnection);
+	socket.addEventListener('close', closeConnection);
+	socket.addEventListener('message', messageRecieved);
+}
+
+function openConnection() {
+	// display the change of state:
+	console.log((new Date()) + ' Client connected');
+  }
+  
+function closeConnection() {
+	// display the change of state:
+	console.log((new Date()) + ' Client connection closer');
+  }
+
+  function messageRecieved(message){
+	console.log((new Date()) + ' Message recieved ' + message.toString());
+  }
+
+function changeConnection(event) {
+	// open the connection if it's closed, or close it if open:
+	if (socket.readyState === WebSocket.CLOSED) {
+	  openSocket(serverURL);
+	} else {
+	  socket.close();
+	}
+  }
+  
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
@@ -21,5 +59,6 @@ var animate = function () {
 	renderer.render(scene, camera);
 };
 
+setup();
 
 animate();
